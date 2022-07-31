4a8e76fee2b67c808afafe8a7b6e4c9690568440
Lingyao5464 <83222166+Lingyao5464@users.noreply.github.com>
2022-07-31 17:07:36 +0800

    Add files via upload

---
+++ 移动端/swiper插件的使用/js/index.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+    // var swiper = new Swiper(".mySwiper", {
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //     },
+    // });
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+})
